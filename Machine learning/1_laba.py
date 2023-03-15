4a8b69e89744a88976b91f2ac9c04f1bbc3b1ff2
erikgett <erikgette63@gmail.com>
2023-03-15 18:39:13 +0500

    1 задача доверительный интервал сделана

--- Machine learning/1_laba.py
+++
@@ -1,7 +0,0 @@
-def disemvowel(string_):
-    voids = ['e','y','u','i','o','a']
-    for char in voids:
-        string_ = string_.replace(char, '').replace(char.upper(), '')
-    return string_
-
-print(disemvowel(string_ ='i< IU>D SuG.dyO g.j r*$Eiu%!uJU'): ')
